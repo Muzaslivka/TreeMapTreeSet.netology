7c0f7ac2d0c989571bad51076661ece8aa63d245
muzaslivka <ms.dmitrieffa@gmail.com>
2023-03-30 18:08:36 +0300

    class Main added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Василий", "Петров", 16));
+        people.add(new Person("Мирослав", "Иванов Петров Сидоров", 12));
+        people.add(new Person("Екатерина", "Королёва Остапенко", 20));
+        people.add(new Person("Виктор", "Певцов Некрасов", 23));
+        people.add(new Person("Владислав", "Громов", 25));
+
+        System.out.println("Неотсортированный список:");
+        System.out.println(people);
+        System.out.println();
+
+        System.out.println("Отсортированный список:");
+        Collections.sort(people, new PersonComparator(3));
+        System.out.println(people);
     }
 }
